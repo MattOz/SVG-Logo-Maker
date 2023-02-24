766a17dcf9ac28cd12a1c5f9fe1579cac9b50577
Matt Osborne <MattJOzzy@gmail.com>
2023-02-23 20:08:32 -0600

    finished functionality, worked on failed tests

--- genLogo.js
+++ genLogo.js
@@ -1,19 +1,24 @@
-function genLogo(response) {
+function genLogo(response) {    
+
+    if (response.shape === "Circle") {
+        response.shape = `<circle cx="150" cy="100" r="80" fill="${response.shapeColor}"/>`
+    }
+
+    if (response.shape === "Square") {
+        response.shape = `<rect x="75" y="25" width="150" height="150" fill="${response.shapeColor}"/> `
+    }
+
+    if (response.shape === "Triangle") {
+        response.shape = `<polygon points="150,25 275,150 25,150" fill="${response.shapeColor}"/>`
+    }
+
     return `<svg version="1.1" width="300" height="200">
 
-    <circle cx="150" cy="100" r="80" fill="${response.color}"/>
+    ${response.shape}
   
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="black">${response.characters}</text>
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.characters}</text>
   
     </svg>`
 }
 
 module.exports = genLogo;
-
-{/* <svg>
-<polygon points="150,25 275,150 25,150"/> 
-<rect x="75" y="25" width="150" height="150"/> 
-<circle cx="150" cy="100" r="80" fill="cyan"/>
-</svg> */}
-
-//refer to document.js in Mini Project
--- index.js
+++ index.js
@@ -3,25 +3,32 @@ const inquirer = require('inquirer');
 const genLogo = require('./genLogo');
 
 inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Enter characters (3 max)',
-            name: 'characters',
-        },
-        {
-            type: 'list',
-            message: 'Select shape',
-            choices: ["Circle","Square","Triangle"],
-            name: 'shape',
-          },
-          {
-            type: 'list',
-            message: 'Select color',
-            choices: ["red","green","blue","black","white"],
-            name: 'color',
-          },
+  {
+    type: 'input',
+    message: 'Enter characters (3 max)',
+    name: 'characters',
+  },
+  {
+    type: 'list',
+    message: 'Select text color',
+    choices: ["red","green","blue","black","white"],
+    name: 'textColor',
+  },
+  {
+    type: 'list',
+    message: 'Select shape',
+    choices: ["Circle","Square","Triangle"],
+    name: 'shape',
+  },
+  {
+    type: 'list',
+    message: 'Select shape color',
+    choices: ["red","green","blue","black","white"],
+    name: 'shapeColor',
+  },
 ])
+
 .then((response) => {
     fs.writeFile(`logo.svg`,genLogo(response) , (err) =>
-    err ? console.error(err) : console.log('Logo Created'))
+    err ? console.error(err) : console.log('Generated logo.svg'))
 });
